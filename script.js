703bffab3ec5404e11699bcf78497ce89b47095b
piercef256 <42844463+piercef256@users.noreply.github.com>
2021-09-22 15:12:17 -0700

    basic features added

--- script.js
+++ script.js
@@ -1,22 +1,66 @@
-const GRID_SIZE = 16;
+let grid_size = 10;
 
-createGrid = () => {
-    const container = document.querySelector(".container");
+createGrid = (classes = "node light") => {
+    const container = document.querySelector(".grid");
+    emptyGridContainerBeforeInsertingNewSizeGrid(container);
 
-    for (var i = 0; i < GRID_SIZE; i++) {
-        var row = document.createElement('div');
+    for (var i = 0; i < grid_size; i++) {
+        var row = document.createElement("div");
         row.className = "row";
-        for (var j = 0; j < GRID_SIZE; j++) {
-            var node = document.createElement('div');
-            node.className = "node";
-            node.style.width = `${1280 / GRID_SIZE / 4}px`;
-            node.style.height = `${1280 / GRID_SIZE / 4}px`;
-            node.id = `${i * GRID_SIZE + 1 + j}`;
+        for (var j = 0; j < grid_size; j++) {
+            var node = document.createElement("div");
+            node.setAttribute("class", `${classes}`);
+            let formula = 500 / grid_size;
+            node.style.width = `${formula}px`;
+            node.style.height = `${formula}px`;
+            node.id = `${i * grid_size + 1 + j}`;
             row.appendChild(node);
         }
         container.appendChild(row);
     }
+    const nodes = Array.from(document.querySelectorAll(".node"));
+    nodes.forEach((node) =>
+        node.addEventListener("mouseover", changeNodeColor)
+    );
+};
 
-}
+changeNodeColor = (e) => {
+    e.target.classList.add("dark");
+};
 
-createGrid();
+resetGridColors = () => {
+    const nodes = document.querySelectorAll(".node");
+    nodes.forEach((node) => node.setAttribute("class", "node light"));
+};
+
+emptyGridContainerBeforeInsertingNewSizeGrid = (container) => {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+};
+
+updateSliderSizeDisplayOnMouseMove = (e) => {
+    const grid_size_display = document.querySelector("#grid-size-display");
+    grid_size_display.textContent = `${e.target.value} x ${e.target.value}`;
+};
+
+createGridOfNewSliderSize = (e) => {
+    grid_size = e.target.value;
+    createGrid();
+};
+
+addEventListenersToElements = () => {
+    const reset = document.querySelector("#reset-button");
+    reset.addEventListener("click", resetGridColors);
+
+    const slider = document.querySelector("#slider");
+    slider.addEventListener("change", createGridOfNewSliderSize);
+    slider.addEventListener("mousemove", updateSliderSizeDisplayOnMouseMove);
+};
+
+main = () => {
+    createGrid();
+    addEventListenersToElements();
+};
+
+main();
